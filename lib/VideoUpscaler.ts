6c192cc702082467e0f8f93f7a5b4add3c777657
Alexey D. Gusev <alex-gusev@yandex-team.ru>
2021-12-29 16:40:59 +0300

    add destroy()

--- lib/VideoUpscaler.ts
+++ lib/VideoUpscaler.ts
@@ -27,195 +27,219 @@ export class VideoUpscaler {
     private canvasHidden: boolean | undefined;
 
     private targetFPS: number;
-    private fps: number;
-    private prevFrameRenderTime: DOMHighResTimeStamp | undefined;
+    private fps = 0;
+    private prevFrameRenderTime: DOMHighResTimeStamp = -1;
+
+    private destroyHandlers: Array<() => unknown> = [() => this.disable()];
+    private destroyed = false;
 
     constructor({ video, canvas, onFrameRendered, targetFPS = DEFAULT_TARGET_FPS }: VideoUpscalerProps) {
-        this.video = video;
-        this.canvas = canvas;
-        this.fps = this.targetFPS = targetFPS;
-        this.hideCanvas();
+        try {
+            this.video = video;
+            this.canvas = canvas;
+            this.onFrameRendered = onFrameRendered;
+            this.targetFPS = targetFPS;
 
-        this.onFrameRendered = onFrameRendered;
+            this.hideCanvas();
 
-        this.video.addEventListener('timeupdate', () => {
-            if (!this.video.paused) {
-                return;
+            const gl = canvas.getContext('webgl2')!;
+
+            if (!gl) {
+                throw new Error('WebGL2 is not supported');
             }
-            this.planNextRender();
-        });
+            this.gl = gl;
+            this.onDestroy(() => gl.getExtension('WEBGL_lose_context')!.loseContext());
 
-        this.observer = new ResizeObserver(([{ contentRect }]) => {
-            this.videoTagWidth = contentRect.width * devicePixelRatio;
-            this.videoTagHeight = contentRect.height * devicePixelRatio;
-        });
-        this.observer.observe(video);
+            const program = gl.createProgram();
+            this.onDestroy(() => gl.deleteProgram(program));
 
-        const gl = canvas.getContext('webgl2')!;
+            const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+            const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+            this.onDestroy(() => gl.deleteShader(vertexShader));
+            this.onDestroy(() => gl.deleteShader(fragmentShader));
 
-        if (!gl) {
-            throw new Error('WebGL2 is not supported');
-        }
-        this.gl = gl;
-
-        const program = gl.createProgram();
-        const vertexShader = gl.createShader(gl.VERTEX_SHADER);
-        const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-
-        if (program === null) {
-            throw new Error('Failed to create program');
-        }
-        if (vertexShader === null) {
-            throw new Error(`Failed to create vertex shader`);
-        }
-        if (fragmentShader === null) {
-            throw new Error(`Failed to create fragment shader`);
-        }
-
-        gl.shaderSource(vertexShader, VERTEX_SHADER_SOURCE);
-        gl.compileShader(vertexShader);
-        gl.shaderSource(fragmentShader, FRAGMENT_SHADER_SOURCE);
-        gl.compileShader(fragmentShader);
-        gl.attachShader(program, vertexShader);
-        gl.attachShader(program, fragmentShader);
-        gl.linkProgram(program);
-
-        // Check the link status
-        const linked = gl.getProgramParameter(program, gl.LINK_STATUS);
-        if (!linked) {
-            // something went wrong with the link
-            const programError = gl.getProgramInfoLog(program);
-            const vertexShaderError = gl.getShaderInfoLog(vertexShader);
-            const fragmentShaderError = gl.getShaderInfoLog(fragmentShader);
-
-            gl.deleteShader(vertexShader);
-            gl.deleteShader(fragmentShader);
-            gl.deleteProgram(program);
-            let errorMessage = 'Failed to link program\n';
-            if (programError) {
-                errorMessage += `programError: ${programError}\n`;
+            if (program === null) {
+                throw new Error('Failed to create program');
             }
-            if (vertexShaderError) {
-                errorMessage += `vertexShaderError: ${vertexShaderError}\n`;
+            if (vertexShader === null) {
+                throw new Error(`Failed to create vertex shader`);
             }
-            if (fragmentShaderError) {
-                errorMessage += `fragmentShaderError: ${fragmentShaderError}\n`;
+            if (fragmentShader === null) {
+                throw new Error(`Failed to create fragment shader`);
             }
-            throw new Error(errorMessage);
+
+            gl.shaderSource(vertexShader, VERTEX_SHADER_SOURCE);
+            gl.compileShader(vertexShader);
+            gl.shaderSource(fragmentShader, FRAGMENT_SHADER_SOURCE);
+            gl.compileShader(fragmentShader);
+            gl.attachShader(program, vertexShader);
+            gl.attachShader(program, fragmentShader);
+            gl.linkProgram(program);
+
+            // Check the link status
+            const linked = gl.getProgramParameter(program, gl.LINK_STATUS);
+            if (!linked) {
+                // something went wrong with the link
+                const programError = gl.getProgramInfoLog(program);
+                const vertexShaderError = gl.getShaderInfoLog(vertexShader);
+                const fragmentShaderError = gl.getShaderInfoLog(fragmentShader);
+
+                gl.deleteShader(vertexShader);
+                gl.deleteShader(fragmentShader);
+                gl.deleteProgram(program);
+                let errorMessage = 'Failed to link program\n';
+                if (programError) {
+                    errorMessage += `programError: ${programError}\n`;
+                }
+                if (vertexShaderError) {
+                    errorMessage += `vertexShaderError: ${vertexShaderError}\n`;
+                }
+                if (fragmentShaderError) {
+                    errorMessage += `fragmentShaderError: ${fragmentShaderError}\n`;
+                }
+                throw new Error(errorMessage);
+            }
+
+            // look up where the vertex data needs to go.
+            const positionAttributeLocation = gl.getAttribLocation(
+                program,
+                'a_position',
+            );
+            const texCoordAttributeLocation = gl.getAttribLocation(
+                program,
+                'a_texCoord',
+            );
+
+            // lookup uniforms
+            this.resolutionLocation = gl.getUniformLocation(
+                program,
+                'u_resolution',
+            );
+            const imageLocation = gl.getUniformLocation(program, 'u_image');
+
+            // Create a vertex array object (attribute state)
+            const vao = gl.createVertexArray();
+            this.onDestroy(() => gl.deleteVertexArray(vao));
+
+            // and make it the one we're currently working with
+            gl.bindVertexArray(vao);
+
+            // Create a buffer and put a single pixel space rectangle in
+            // it (2 triangles)
+            const positionBuffer = gl.createBuffer();
+            this.onDestroy(() => gl.deleteBuffer(positionBuffer));
+
+            // Turn on the attribute
+            gl.enableVertexAttribArray(positionAttributeLocation);
+
+            // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
+            gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+            // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+            gl.vertexAttribPointer(
+                positionAttributeLocation,
+                2,
+                gl.FLOAT,
+                false,
+                0,
+                0,
+            );
+
+            // provide texture coordinates for the rectangle.
+            const arrayBuffer = gl.createBuffer();
+            this.onDestroy(() => gl.deleteBuffer(arrayBuffer));
+            gl.bindBuffer(gl.ARRAY_BUFFER, arrayBuffer);
+            gl.bufferData(
+                gl.ARRAY_BUFFER,
+                new Float32Array([0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1]),
+                gl.STATIC_DRAW,
+            );
+
+            // Turn on the attribute
+            gl.enableVertexAttribArray(texCoordAttributeLocation);
+
+            // Tell the attribute how to get data out of texCoordBuffer (ARRAY_BUFFER)
+            gl.vertexAttribPointer(
+                texCoordAttributeLocation,
+                2,
+                gl.FLOAT,
+                false,
+                0,
+                0,
+            );
+
+            // Create a texture.
+            const texture = gl.createTexture();
+            this.onDestroy(() => gl.deleteTexture(texture));
+
+            // make unit 0 the active texture uint
+            // (ie, the unit all other texture commands will affect
+            gl.activeTexture(gl.TEXTURE0);
+
+            // Bind it to texture unit 0' 2D bind point
+            gl.bindTexture(gl.TEXTURE_2D, texture);
+
+            // Set the parameters so we don't need mips and so we're not filtering
+            // and we don't repeat at the edges
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+
+            // Tell it to use our program (pair of shaders)
+            gl.useProgram(program);
+
+            // Bind the attribute/buffer set we want.
+            gl.bindVertexArray(vao);
+
+            // Tell the shader to get the texture from texture unit 0
+            gl.uniform1i(imageLocation, 0);
+
+            // Bind the position buffer so gl.bufferData that will be called
+            // in setRectangle puts data in the position buffer
+            gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+            this.observer = new ResizeObserver(([{ contentRect }]) => this.saveVideoTagSize(contentRect));
+            this.observer.observe(video);
+            this.onDestroy(() => {
+                this.observer.disconnect();
+            })
+
+            const onTimeupdate =  () => {
+                if (!this.video.paused) {
+                    return;
+                }
+                this.planNextRender();
+            };
+            this.video.addEventListener('timeupdate', onTimeupdate);
+            this.onDestroy(() => {
+                this.video.removeEventListener('timeupdate', onTimeupdate);
+            })
+        } catch (e) {
+            this.destroy();
+            throw e;
         }
+    }
 
-        // look up where the vertex data needs to go.
-        const positionAttributeLocation = gl.getAttribLocation(
-            program,
-            'a_position',
-        );
-        const texCoordAttributeLocation = gl.getAttribLocation(
-            program,
-            'a_texCoord',
-        );
-
-        // lookup uniforms
-        this.resolutionLocation = gl.getUniformLocation(
-            program,
-            'u_resolution',
-        );
-        const imageLocation = gl.getUniformLocation(program, 'u_image');
-
-        // Create a vertex array object (attribute state)
-        const vao = gl.createVertexArray();
-
-        // and make it the one we're currently working with
-        gl.bindVertexArray(vao);
-
-        // Create a buffer and put a single pixel space rectangle in
-        // it (2 triangles)
-        const positionBuffer = gl.createBuffer();
-
-        // Turn on the attribute
-        gl.enableVertexAttribArray(positionAttributeLocation);
-
-        // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
-        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
-        // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
-        gl.vertexAttribPointer(
-            positionAttributeLocation,
-            2,
-            gl.FLOAT,
-            false,
-            0,
-            0,
-        );
-
-        // provide texture coordinates for the rectangle.
-        gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
-        gl.bufferData(
-            gl.ARRAY_BUFFER,
-            new Float32Array([0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1]),
-            gl.STATIC_DRAW,
-        );
-
-        // Turn on the attribute
-        gl.enableVertexAttribArray(texCoordAttributeLocation);
-
-        // Tell the attribute how to get data out of texCoordBuffer (ARRAY_BUFFER)
-        gl.vertexAttribPointer(
-            texCoordAttributeLocation,
-            2,
-            gl.FLOAT,
-            false,
-            0,
-            0,
-        );
-
-        // Create a texture.
-        const texture = gl.createTexture();
-
-        // make unit 0 the active texture uint
-        // (ie, the unit all other texture commands will affect
-        gl.activeTexture(gl.TEXTURE0);
-
-        // Bind it to texture unit 0' 2D bind point
-        gl.bindTexture(gl.TEXTURE_2D, texture);
-
-        // Set the parameters so we don't need mips and so we're not filtering
-        // and we don't repeat at the edges
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-
-        // Tell it to use our program (pair of shaders)
-        gl.useProgram(program);
-
-        // Bind the attribute/buffer set we want.
-        gl.bindVertexArray(vao);
-
-        // Tell the shader to get the texture from texture unit 0
-        gl.uniform1i(imageLocation, 0);
-
-        // Bind the position buffer so gl.bufferData that will be called
-        // in setRectangle puts data in the position buffer
-        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    private saveVideoTagSize({ width, height }: Pick<DOMRect, 'width' | 'height'>): void {
+        this.videoTagWidth = Math.round(width * devicePixelRatio);
+        this.videoTagHeight = Math.round(height * devicePixelRatio);
     }
 
     private getVideoTagSize(): Pick<DOMRect, 'width' | 'height'> {
-        let { videoTagWidth: width, videoTagHeight: height } = this;
-        if (width === undefined || height === undefined) {
-            const videoRect = this.video.getBoundingClientRect();
-            width = this.videoTagWidth = videoRect.width * devicePixelRatio;
-            height = this.videoTagHeight = videoRect.height * devicePixelRatio;
+        if (this.videoTagWidth === undefined || this.videoTagHeight === undefined) {
+            this.saveVideoTagSize(this.video.getBoundingClientRect());
         }
 
         return {
-            width,
-            height,
+            width: this.videoTagWidth!,
+            height: this.videoTagHeight!,
         };
     }
 
     private clearFPSCounter(): void {
         this.fps = this.targetFPS;
-        this.prevFrameRenderTime = undefined;
+        this.prevFrameRenderTime = -1;
     }
 
     private hideCanvas(): void {
@@ -237,7 +261,7 @@ export class VideoUpscaler {
         this.canvasHidden = false;
     }
 
-    private setGlViewportSize({
+    private setGLViewportSize({
         width,
         height,
     }: Pick<DOMRect, 'width' | 'height'>): void {
@@ -303,7 +327,7 @@ export class VideoUpscaler {
             canvas.width !== desiredFrameSize.width ||
             canvas.height !== desiredFrameSize.height
         ) {
-            this.setGlViewportSize(desiredFrameSize);
+            this.setGLViewportSize(desiredFrameSize);
         }
 
         gl.texImage2D(
@@ -324,10 +348,10 @@ export class VideoUpscaler {
             this.onFrameRendered();
         }
        
-        if (this.prevFrameRenderTime !== undefined && !video.paused) {
+        if (this.prevFrameRenderTime >= 0 && !video.paused) {
             const delta = (now - this.prevFrameRenderTime) / 1000;
             this.fps = this.fps * 0.95 + (0.05 * 1) / delta;
-            console.log(this.fps/this.targetFPS);
+            // console.log(this.fps/this.targetFPS);
         }
         this.prevFrameRenderTime = now;
 
@@ -341,6 +365,9 @@ export class VideoUpscaler {
     };
 
     public enable(): void {
+        if (this.destroyed) {
+            return;
+        }
         this.hideCanvas();
         this.renderFrame();
         this.planNextRender();
@@ -353,6 +380,10 @@ export class VideoUpscaler {
         this.video.cancelVideoFrameCallback(this.nextRFCHandle);
     }
 
+    private onDestroy(handler: () => unknown): void {
+        this.destroyHandlers.push(handler);
+    }
+
     public disable(): void {
         this.hideCanvas();
         this.cancelNextRender();
@@ -360,12 +391,17 @@ export class VideoUpscaler {
     }
 
     public destroy(): void {
-        this.disable();
-        this.observer.disconnect();
-        try {
-            this.gl.getExtension('WEBGL_lose_context')!.loseContext();
-        } catch {
-            //
+        if (this.destroyed) {
+            return;
         }
+
+        this.destroyHandlers.forEach((handler) => {
+            try {
+                handler()
+            } catch {
+                //
+            }
+        });
+        this.destroyed = true;
     }
 }
