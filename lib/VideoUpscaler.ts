5b967fa7138ef9b0d043e96e0c7bf8a13223ba20
Alexey D. Gusev <alex-gusev@yandex-team.ru>
2022-01-10 16:06:21 +0300

    move texture setup

--- lib/VideoUpscaler.ts
+++ lib/VideoUpscaler.ts
@@ -130,24 +130,6 @@ export class VideoUpscaler {
                 0,
             );
 
-            // Create a texture.
-            const texture = gl.createTexture();
-            this.onDestroy(() => gl.deleteTexture(texture));
-
-            // make unit 0 the active texture uint
-            // (ie, the unit all other texture commands will affect
-            gl.activeTexture(gl.TEXTURE0);
-
-            // Bind it to texture unit 0' 2D bind point
-            gl.bindTexture(gl.TEXTURE_2D, texture);
-
-            // Set the parameters so we don't need mips and so we're not filtering
-            // and we don't repeat at the edges
-            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-
             // Tell it to use our program (pair of shaders)
             gl.useProgram(program);
 
@@ -234,6 +216,25 @@ export class VideoUpscaler {
             }
             throw new Error(errorMessage);
         }
+
+        // Create a texture.
+        const texture = gl.createTexture();
+        this.onDestroy(() => gl.deleteTexture(texture));
+
+        // make unit 0 the active texture uint
+        // (ie, the unit all other texture commands will affect
+        gl.activeTexture(gl.TEXTURE0);
+
+        // Bind it to texture unit 0' 2D bind point
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+
+        // Set the parameters so we don't need mips and so we're not filtering
+        // and we don't repeat at the edges
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+
 
         return program;
     }
